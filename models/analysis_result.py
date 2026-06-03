4e30e32aa9133dae7d71a99261e612f6ab61b64f
Gabriel Garcia <ggarcia@trunorthcyber.com>
2026-06-03 14:08:32 -0400

    Added IOC extractor to analysis_result script

--- models/analysis_result.py
+++ models/analysis_result.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import List
+from typing import List, Dict
 
 from models.finding import Finding
 
@@ -9,3 +9,12 @@ class AnalysisResult:
     verdict: str
     total_score: int
     findings: List[Finding] = field(default_factory=list)
+
+    indicators: Dict[str, list] = field(
+        default_factory=lambda: {
+            "urls": [],
+            "domains": [],
+            "ips": [],
+            "hashes": []
+        }
+    )
